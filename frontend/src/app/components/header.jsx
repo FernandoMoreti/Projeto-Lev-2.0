5da8fb370a66007a036e83f1f35415ebae311d0b
FernandoMoreti <fernandombolela@gmail.com>
2025-10-14 15:38:33 -0300

    feat(page) Adicionando funcionalidade para que o upload leia e mostre as colunas da tabela

--- frontend/src/app/components/header.jsx
+++ frontend/src/app/components/header.jsx
@@ -2,7 +2,7 @@ import { IoDocumentTextOutline  } from "react-icons/io5";
 
 export default function Header() {
     return (
-        <header className="flex items-center py-5 px-7 gap-5 border-b-1 border-gray-300">
+        <header className="flex items-center py-5 px-7 gap-5 bg-white border-b-1 border-gray-300">
             <div>
                 <h1 className="bg-gradient-to-br from-purple-700 to-purple-400 p-2 rounded-xl">{<IoDocumentTextOutline  size={30} color="white" />}</h1>
             </div>
--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,15 +1,18 @@
 "use client";
 
-import Image from "next/image";
+import { useState } from "react";
+import * as XLSX from "xlsx";
+
 import Header from "./components/header";
 import Input from "./components/Input";
 import { LuUpload } from "react-icons/lu";
 import { FiSave } from "react-icons/fi";
-import { useState } from "react";
 
 export default function Home() {
 
-  const [name, setName] = useState()
+  const [name, setName] = useState("")
+  const [file, setFile] = useState([])
+  const [columns, setColumns] = useState([]);
 
   const col_opcoes = [
     "NUM_BANCO",
@@ -44,12 +47,40 @@ export default function Home() {
     "PCL_TAXA_EMPRESTIMO"
   ]
 
+  const handleFileUpload = (e) => {
+    const file = e.target.files[0];
+    if (!file) return;
+
+    const reader = new FileReader();
+
+    reader.onload = (event) => {
+      const data = new Uint8Array(event.target.result);
+      const workbook = XLSX.read(data, { type: "array" });
+
+      const sheetName = workbook.SheetNames[0];
+      const worksheet = workbook.Sheets[sheetName];
+
+      const jsonData = XLSX.utils.sheet_to_json(worksheet, { defval: "" });
+
+      setFile(jsonData);
+
+      // Pega os nomes das colunas da primeira linha
+      if (jsonData.length > 0) {
+        const colunas = Object.keys(jsonData[0]);
+        setColumns(colunas);
+      }
+    };
+
+    reader.readAsArrayBuffer(file);
+  };
+
+
   return (
     <>
       <Header />
       <section className="grid grid-cols-2 p-10 gap-2">
         <div className="flex flex-col gap-5">
-          <div className="border-1 border-gray-300 p-5 rounded-xl shadow-xl">
+          <div className="border-1 bg-white border-gray-300 p-5 rounded-xl shadow-xl">
             <h1 className="flex items-center text-xl font-semibold gap-2"><LuUpload size={20} className="text-purple-500" /> Upload do Arquivo</h1>
             <p className="text-sm text-gray-500 pt-1 pb-5">Selecione um arquivo Excel (.xlsx, .xls)</p>
             <label
@@ -63,22 +94,27 @@ export default function Home() {
                 id="file-upload"
                 type="file"
                 accept=".xlsx,.xls"
+                onChange={handleFileUpload}
                 className="hidden"
               />
             </label>
           </div>
-          <div className="border-1 border-gray-300 p-5 rounded-xl shadow-xl">
-            <h1 className="flex items-center text-lg font-semibold">Coluna so sistema</h1>
+          <div className="border-1 bg-white border-gray-300 p-5 rounded-xl shadow-xl">
+            <h1 className="flex items-center text-lg font-semibold">Coluna do Upload</h1>
             <p className="text-sm text-gray-500 pt-1 pb-1">Colunas disponiveis para mapeamento</p>
             {
-              col_opcoes.map((col) => (
-                <p className="border-1 border-gray-200 inset-shadow-2xs inset-shadow-gray-400 py-2 px-5 rounded-xl mt-4">{col}</p>
-              ))
+              columns.length > 0 ? (
+                columns.map((col) => (
+                  <p className="border-1 border-gray-200 inset-shadow-2xs inset-shadow-gray-400 py-2 px-5 rounded-xl mt-4">{col}</p>
+                ))
+              ) : (
+                <p className="text-sm text-gray-400">Nenhum arquivo carregado.</p>
+              )
             }
           </div>
         </div>
         <div className="flex flex-col gap-5">
-            <div className="border-1 border-gray-300 p-5 rounded-xl shadow-xl">
+            <div className="border-1 bg-white border-gray-300 p-5 rounded-xl shadow-xl">
               <h1 className="flex items-center text-xl font-semibold gap-2"><FiSave size={20} className="text-purple-500" /> Salvar Mapeamento</h1>
               <p className="text-sm text-gray-500 pt-1 pb-5">Dê um nome para este mapeamento</p>
               <p className="flex items-center text-sm font-semibold pb-2">Nome do Mapeamento</p>
